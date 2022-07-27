e8b3cb9d2283c54720cb907e2914e8cf09d463eb
bundi-xris <bundichris1400@gmail.com>
2022-07-27 16:21:16 +0300

    Removed a print on test_main file

--- test_main.py
+++ test_main.py
@@ -18,7 +18,7 @@ SECRET = os.getenv('JWT_SECRET')
 TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1NjEzMDY3OTAsIm5iZiI6MTU2MDA5NzE5MCwiZW1haWwiOiJ3b2xmQHRoZWRvb3IuY29tIn0.IpM4VMnqIgOoQeJxUbLT-cRcAjK41jronkVrqRLFmmk'
 EMAIL = 'wolf@thedoor.com'
 PASSWORD = 'huff-puff'
-print(os.getenv('LOG_LEVEL'))
+# print(os.getenv('LOG_LEVEL'))
     
 @pytest.fixture
 def client():
